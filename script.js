dc5687a1cf13975c3997951879fa9d3368f23e86
Danilo César <dkdanjolly@gmail.com>
2026-07-17 00:07:42 -0300

    Fix SVG path in edit button HTML

--- script.js
+++ script.js
@@ -434,7 +434,7 @@ document.addEventListener("click", (e) => {
 const editButton = `
   <button class="btnEdit" title="Editar projeto">
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 24 24">
-      <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+      <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
     </svg>
   </button>
 `;
